8889595641455225ac26c68fcbd125a05f1eadb8
wasimud <ultimodazn7@gmail.com>
2026-02-11 15:17:55 +0100

    Update amstaff.py

--- amstaff.py
+++ amstaff.py
@@ -9,7 +9,7 @@ import requests
 
 DEBUG = True
 
-DEFAULT_LOGO = "https://upload.wikimedia.org/wikipedia/commons/e/e3/Tv-channel-%E2%94%82-dazn-1.png"
+DEFAULT_LOGO = "https://toppng.com/uploads/preview/3790038-logo-vetrina-dazn-sport-dazn-a-11563364038i3yzrattgk.png"
 OUTPUT_M3U = "sport_lastminute.m3u8"
 
 HOME_URL = "https://test34344.herokuapp.com/filter.php"
