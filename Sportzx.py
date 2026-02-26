17d6c6635f90a96290b874ece0253b64d7e39f51
wasimud <ultimodazn7@gmail.com>
2026-02-26 20:25:10 +0100

    Update Sportzx.py

--- Sportzx.py
+++ Sportzx.py
@@ -338,7 +338,7 @@ if __name__ == "__main__":
         client.generate_m3u(
             channels=canali,
             filename="Sportzx.m3u8",
-            generic_logo="https://i.postimg.cc/xdhSY2xq/does-anyone-have-transparent-sportzx-icons-they-can-share-v0-wyufeqaxobff1.png"
+            generic_logo="https://upload.wikimedia.org/wikipedia/commons/c/c2/Serie_A.png"
         )
     else:
         print("Nessun canale trovato")
