e5b4ce090ed333e438c012e379337cf746284864
wasimud <ultimodazn7@gmail.com>
2026-03-28 20:39:05 +0100

    Update Youtube.py

--- Youtube.py
+++ Youtube.py
@@ -17,6 +17,14 @@ channels = [
         "url": "https://www.youtube.com/@xMurryPwNz/videos"
     },
     {
+        "name": "iNoob Gaming",
+        "url": "https://www.youtube.com/@iNoobGaming/videos"
+    },
+    {
+        "name": "Fabrizio Corona",
+        "url": "https://www.youtube.com/@officialfabriziocorona/videos"
+    },
+    {
         "name": "Favij",
         "url": "https://www.youtube.com/@FavijTV/videos"
     },
@@ -29,6 +37,10 @@ channels = [
         "url": "https://www.youtube.com/@stockdroid/videos"
     },
     {
+        "name": "X2Marco",
+        "url": "https://www.youtube.com/@X2Marco/videos"
+    },
+    {
         "name": "Leo",
         "url": "https://www.youtube.com/@leo/videos"
     },
@@ -37,12 +49,156 @@ channels = [
         "url": "https://www.youtube.com/@LaSediaa2Gambe/videos"
     },
     {
+        "name": "La Sedia Da Gaming",
+        "url": "https://www.youtube.com/@lasediadagaming/videos"
+    },
+    {
+        "name": "Poldo",
+        "url": "https://www.youtube.com/@poldo/videos"
+    },
+    {
+        "name": "Poldo Extra",
+        "url": "https://www.youtube.com/@PoldoExtra/videos"
+    },
+    {
+        "name": "Michele Molteni",
+        "url": "https://www.youtube.com/@MolteniMichele/videos"
+    },
+    {
+        "name": "Gabriele Vagnato",
+        "url": "https://www.youtube.com/@gabrielevagnato/videos"
+    },
+    {
+        "name": "Jakidale",
+        "url": "https://www.youtube.com/@jakidale/videos"
+    },
+    {
+        "name": "Techdale",
+        "url": "https://www.youtube.com/@TechDale/videos"
+    },
+    {
+        "name": "YungestMoonstar",
+        "url": "https://www.youtube.com/@ymoonstar/videos"
+    },
+    {
+        "name": "Thasup",
+        "url": "https://www.youtube.com/@thasup/videos"
+    },
+    {
+        "name": "Cicciogamer89",
+        "url": "https://www.youtube.com/@CiccioGamer89/videos"
+    },
+    {
+        "name": "Cicciogamer89 GamePlay",
+        "url": "https://www.youtube.com/@CiccioGameplay89/videos"
+    },
+    {
+        "name": "Grax",
+        "url": "https://www.youtube.com/@Grax/videos"
+    },
+    {
+        "name": "ControCalcio",
+        "url": "https://www.youtube.com/@Controcalcio/videos"
+    },
+    {
+        "name": "Riccardo Dose",
+        "url": "https://www.youtube.com/@RiccardoDose94/videos"
+    },
+    {
+        "name": "Nicolo Cumerlato",
+        "url": "https://www.youtube.com/@nico.lato/videos"
+    },
+    {
+        "name": "Giovanni Fois",
+        "url": "https://www.youtube.com/@GiovanniFois/videos"
+    },
+    {
+        "name": "Lollo Lacustre",
+        "url": "https://www.youtube.com/@lollolacustre/videos"
+    },
+    {
+        "name": "Aledellagiusta",
+        "url": "https://www.youtube.com/@aledellagiusta/videos"
+    },
+    {
+        "name": "Kappa Freneh",
+        "url": "https://www.youtube.com/@Frenezy/videos"
+    },
+    {
+        "name": "Il Ridoppiatore",
+        "url": "https://www.youtube.com/@ILRidoppiatore/videos"
+    },
+    {
+        "name": "Dado",
+        "url": "https://www.youtube.com/@dadontheroad/videos"
+    },
+    {
+        "name": "Andrea Galeazzi",
+        "url": "https://www.youtube.com/@AndreaGaleazziVERIFICATO/videos"
+    },
+    {
+        "name": "MikeShowSha",
+        "url": "https://www.youtube.com/@MikeShowSha/videos"
+    },
+    {
+        "name": "Blur Freebooter",
+        "url": "https://www.youtube.com/@stalla_freebooter/videos"
+    },
+    {
+        "name": "Surry",
+        "url": "https://www.youtube.com/@surrealpower/videos"
+    },
+    {
+        "name": "Surry And Chills",
+        "url": "https://www.youtube.com/@SurryAndChill/videos"
+    },
+    {
+        "name": "Dario Moccia Archives",
+        "url": "https://www.youtube.com/@DarioMocciaArchives/videos"
+    },
+    {
+        "name": "NerdMovieProductions",
+        "url": "https://www.youtube.com/@NerdMovieProducti0ns/videos"
+    },
+    {
+        "name": "Otto Climan",
+        "url": "https://www.youtube.com/@OttoCliman/videos"
+    },
+    {
+        "name": "ZW Jackson",
+        "url": "https://www.youtube.com/@ZWJACKSON/videos"
+    },
+    {
+        "name": "Fc Zeta Milano",
+        "url": "https://www.youtube.com/@FCZetaMilano/videos"
+    },
+    {
+        "name": "St3pNy",
+        "url": "https://www.youtube.com/@MoD3rNSt3pNy/videos"
+    },
+    {
+        "name": "St3pNy²",
+        "url": "https://www.youtube.com/@St3pNy/videos"
+    },
+    {
+        "name": "Techdale",
+        "url": "https://www.youtube.com/@TechDale/videos"
+    },
+    {
+        "name": "Stallions TV Blur",
+        "url": "https://www.youtube.com/@StallionsTV/videos"
+    },
+    {
         "name": "Blur",
         "url": "https://www.youtube.com/@blurindietro/videos"
     },
     {
         "name": "Marza",
         "url": "https://www.youtube.com/@marzatv/videos"
+    },
+    {
+        "name": "The Real Marza",
+        "url": "https://www.youtube.com/@TheRealMarzaa/videos"
     },
     {
         "name": "Stormy",
