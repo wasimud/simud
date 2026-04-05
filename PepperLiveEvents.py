a61905219eada58f16c22debab722aa284d775e9
wasimud <ultimodazn7@gmail.com>
2026-04-05 12:52:01 +0200

    Update PepperLiveEvents.py

--- PepperLiveEvents.py
+++ PepperLiveEvents.py
@@ -26,7 +26,7 @@ import cloudscraper
 # CONFIG
 # =============================================
 
-BASE = "https://pepperlive.info"
+BASE = "https://www.chilistream.net/"
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36",
     "Referer": BASE + "/",
